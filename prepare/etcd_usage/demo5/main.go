5aafb80d6ef215dbebd635843dde5447af9ec16c
Jason <jason@JasondeMacBook-Pro.local>
2019-02-17 23:13:11 +0800

    3 ~ 4-9

---
+++ prepare/etcd_usage/demo5/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/mvcc/mvccpb"
+	"time"
+)
+
+func main() {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		delResp *clientv3.DeleteResponse
+		kvpair  *mvccpb.KeyValue
+	)
+
+	config = clientv3.Config{
+		Endpoints:   []string{"0.0.0.0:2379"}, // 集群列表
+		DialTimeout: 5 * time.Second,
+	}
+
+	// 建立一个客户端
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 操作etcd,用于读写etcd的键值对
+	kv = clientv3.NewKV(client)
+
+	// 删除KV
+	if delResp, err = kv.Delete(context.TODO(), "/cron/jobs/job2", clientv3.WithPrevKV()); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 被删除之前的value是什么
+	if len(delResp.PrevKvs) != 0 {
+		for _, kvpair = range delResp.PrevKvs {
+			fmt.Println("删除了:", string(kvpair.Key), string(kvpair.Value))
+		}
+	}
+
+}
